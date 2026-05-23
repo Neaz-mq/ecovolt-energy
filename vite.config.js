eef08d4dfe8fdcb39b22618ac16d71f527ab5412
Neaz-mq <neazmorshed666@gmail.com>
2026-05-23 17:05:04 +0600

    work navbar

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
-  publicDir: resolve(__dirname, 'public'), // ← ADDED: points to project-root/public/
+  publicDir: resolve(__dirname, 'public'), 
   build: {
     outDir: '../dist',
     emptyOutDir: true,
